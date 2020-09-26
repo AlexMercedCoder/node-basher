528ed4e758b88055ed2ca862a376ff3e5355beb3
Alex Merced <willcodeforfood@alexmerced.com>
2020-09-26 18:40:22 -0400

    update

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const chalk = require("chalk");
 
 const [, , script, $1, $2, $3, $4] = process.argv;
 
-console.log($1, $2)
+console.log($1, $2);
 
 if (script === "spinup") {
   const temp = templates[$1] ? templates[$1] : templates["reactwebp"];
   const name = $2 ? $2 : "myproject";
 
-  spawn(`./spinup.bash ${temp} ${name}`).then(() =>
+  spawn(
+    `git clone --depth 1 ${temp} ${name} && cd ${name} && git remote rm origin && echo "Installing Dependencies" && npm install`
+  ).then(() =>
     console.log(
       chalk.bgCyan.black(
         "Project Created, Enjoy! Make sure to join the Slack and Discord communities at devNursery.com and subscribe to my youtube channel at AlexMercedCoder.com"
